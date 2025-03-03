9db7951db6e081f662bbc969c0ff191fca450f72
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-03-03 16:37:37 -0300

    adição de futuras adições

---
+++ Service/imagem.service.js
@@ -0,0 +1,20 @@
+// imagem.service/imagemService.js
+/*
+const ImagemRepository = require('../Repository/imageRepository.js');
+
+exports.cadastroImagens = async (referencia, titulo, data_criacao) => {
+    return await ImagemRepository.create(referencia, titulo, data_criacao);
+};
+
+exports.encontrarImagem = async (id) => {
+    return await ImagemRepository.findById(id);
+};
+
+exports.atualizarImagem = async (id, referencia, titulo) => {
+    return await ImagemRepository.update(id, referencia, titulo);
+};
+
+exports.deletarImagem = async (id) => {
+    return await ImagemRepository.delete(id);
+};
+*/
