e8eab397d470cf4275ec3f1af5bdbae62fd5fc44
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-03-03 17:50:51 -0300

    testes

--- Controller/aws.Controller.js
+++ Controller/aws.Controller.js
@@ -49,30 +49,3 @@ exports.downloadFile = async (req, res) => {
 };
 
 
-/*
-const multer = require('multer');
-const awsService = require('../aws.service/awsService');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-exports.uploadFile = async (req, res) => {
-    try {
-        const result = await awsService.uploadFile(req.file);
-        res.status(200).json({ message: 'Upload realizado com sucesso', ...result });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-exports.downloadFile = async (req, res) => {
-    try {
-        const fileStream = awsService.downloadFile(req.params.key);
-        res.attachment(req.params.key);
-        fileStream.pipe(res);
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao baixar o arquivo', error: error.message });
-    }
-};
-
-*/
